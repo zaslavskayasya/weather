66ba2d01f4aff146cc3856002d74e0337e59937a
zaslavskayasya <web.zaslavskaya@gmail.com>
2016-11-23 13:31:02 +0200

    Данные выводятся

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ function renderWeather(data){
     /*gitconsole.log(data);*/
     var weather =
         '<p class=""> Погода сейчас : </p>'+
-        '<p class="weatherNow"> '+ data.temp_c +' </p>' +
-        '<p class="weatherNow"> '+ data.last_updated+' </p>' +
+        '<p class="weatherNow"> '+ weatherCurrent.temp_c +' </p>' +
+        '<p class="weatherNow"> '+ weatherCurrent.last_updated+' </p>' +
         '<button class="take_weather">take_weather</button>'
     $('#block').html(weather);
 }
