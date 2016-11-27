ea476828f822b008560b1bc188aa9b0214e23aad
zaslavskayasya <web.zaslavskaya@gmail.com>
2016-11-27 10:47:30 +0200

    some designed

--- index.js
+++ index.js
@@ -2,40 +2,36 @@ var weatherCurrent  ;
 var weatherForecast;
 var hum;
 
-
 $('#block').on('click', '.take_weather' , function () {
     $.when(Weather.getCurrentWeather()).then(function(result) {
         weatherCurrent = result.current;
         renderWeather(weatherCurrent);
-
     });
 });
-
-
 
 function renderWeather(data) {
     console.log(data);
     var weather =
         '<div class="row"> ' +
-        '<h4 class="h4  col-md-4"> Сейчас : </h4>' +
-        '<p class="weatherNow h4 "> ' + weatherCurrent.last_updated + ' </p>' +
+            '<h4 class="h4  col-md-4"> Сейчас : </h4>' +
+            '<p class="weatherNow h4 "> ' + weatherCurrent.last_updated + ' </p>' +
         '</div> ' +
         '<div class="row"> ' +
-        '<p class="h4 col-md-4 info"> Температура С : </p>' +
-        '<p class="weatherNow h4 col-md-2"> ' + data.temp_c + ' </p>' +
+            '<p class="h4 col-md-4 info"> Температура С : </p>' +
+            '<p class="weatherNow h4 col-md-2"> ' + data.temp_c + ' </p>' +
         '</div> ' +
         '<div class="row"> ' +
-        '<p class="h4  col-md-4"> Ощущается как  : </p>' +
-        '<p class="weatherNow h4 col-md-2"> ' + weatherCurrent.feelslike_c + ' </p>' +
+            '<p class="h4  col-md-4"> Ощущается как  : </p>' +
+            '<p class="weatherNow h4 col-md-2"> ' + weatherCurrent.feelslike_c + ' </p>' +
         '<div class="weatherNow h4 col-md-2" id="temperaturePlace">  </div>' +
         '</div> ' +
         '<div class="row"> ' +
-        '<p class="h4  col-md-4"> Скорость ветра : </p>' +
-        '<p class="weatherNow h4 col-md-2"> ' + weatherCurrent.wind_kph + ' </p>' +
+            '<p class="h4  col-md-4"> Скорость ветра : </p>' +
+            '<p class="weatherNow h4 col-md-2"> ' + weatherCurrent.wind_kph + ' </p>' +
         '</div> ' +
         '<div class="row"> ' +
-        '<p class="h4 h4 col-md-4"> Влажность : </p>' +
-        '<p class="weatherNow h4 col-md-2"> ' + weatherCurrent.humidity + ' </p>' +
+            '<p class="h4 h4 col-md-4"> Влажность : </p>' +
+            '<p class="weatherNow h4 col-md-2"> ' + weatherCurrent.humidity + '  </p>' +
         '<div class="weatherNow h4 col-md-2" id="humidityPlace">  </div>' +
         '</div> ' +
 
@@ -72,12 +68,6 @@ function renderWeather(data) {
     temperature();
 }
 
-/*$.when(Weather.getForecastWeather()).then(function(resultt) {
- weatherForecast = resultt.forecast.forecastday[4].date;
- console.log(weatherForecast);
- });*/
-
-
 $('#block').on('click', '.take_forecast' , function () {
     $.when(Weather.getForecastWeather()).then(function(resultt) {
         weatherForecast = resultt.forecast.forecastday[1];
@@ -89,16 +79,16 @@ function renderForecast(data){
     /* console.log(data);*/
     var weather =
        '<div class="row"> ' +
-            '<p class="h4 inline col-md-4">Завтра: </p>'+
-            '<p class="weatherNow col-md-4 h5 "> '+ weatherForecast.date +' </p>' +
+            '<p class="h4  col-md-4">Завтра: </p>'+
+            '<p class="weatherNow h4  "> '+ weatherForecast.date +' </p>' +
        '</div> '+
        '<div class="row"> ' +
             '<p class="h4 col-md-4">Температура С: </p>'+
-            '<p class="weatherNow col-md-4 h5"> '+ data.day.maxtemp_c +' </p>' +
+            '<p class="weatherNow h4 col-md-2"> '+ data.day.maxtemp_c +' </p>' +
        '</div> '+
        '<div class="row"> ' +
             '<p class="h4 col-md-4">Скорость ветра : </p>'+
-            '<p class="weatherNow col-md-4 h5"> '+ data.day.maxwind_kph +' </p>' +
+            '<p class="weatherNow h4 col-md-2"> '+ data.day.maxwind_kph +' </p>' +
             
        '</div> '+
         '<button class="take_forecast btn btn-primary">Обновить</button>'
