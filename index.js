b74f5a4c61431f9cb4c7e929ffb18965003ee252
zaslavskayasya <web.zaslavskaya@gmail.com>
2016-12-03 22:12:38 +0200

    v.2 broken-down design

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ var hum;
 var city = 'Odessa';
 var weatherArr = [];
 
-
 $('#block').on('click', '.take_weather' , function () {
     $.when(Weather.getCurrentWeather()).then(function(result) {
         weatherCurrent = result.current;
@@ -12,7 +11,6 @@ $('#block').on('click', '.take_weather' , function () {
     });
     cityCurrent();
 });
-
 
 function cityCurrent(){
     city = $("#select_city option:selected").val();
@@ -22,46 +20,45 @@ function cityCurrent(){
 function renderWeather(data) {
     console.log(data);
 
-
     var weather =
+        '<div class="col-md-4 col-xs-12 col-sm-6 ">'+
         '<div class="row"> ' +
-        '<h4 class="h4  col-md-4"> Сейчас : </h4>' +
-        '<p class="weatherNow h4 "> ' + weatherCurrent.last_updated + ' </p>' +
+        '<h4 class="h5  col-md-5 col-sm-5 col-xs-5"> Сейчас : </h4>' +
+        '<p class="weatherNow h4 col-md-7 col-sm-7 col-xs-7"> ' + weatherCurrent.last_updated + ' </p>' +
         '</div> ' +
         '<div class="row"> ' +
-        '<p class="h4 col-md-4 info"> Температура С : </p>' +
-        '<p class="weatherNow h4 col-md-2"> ' + data.temp_c + ' </p>' +
+        '<p class="h5 col-md-8 col-sm-8 col-xs-8info"> Температура С : </p>' +
+        '<p class="weatherNow h5 col-md-4 col-sm-4 col-xs-4"> ' + data.temp_c + ' </p>' +
         '</div> ' +
         '<div class="row"> ' +
-        '<p class="h4  col-md-4"> Ощущается как  : </p>' +
-        '<p class="weatherNow h4 col-md-2"> ' + weatherCurrent.feelslike_c + ' </p>' +
+        '<p class="h5  col-md-8 col-sm-8 col-xs-8"> Ощущается как  : </p>' +
+        '<p class="weatherNow h5 col-md-4 col-sm-4 col-xs-4"> ' + weatherCurrent.feelslike_c + ' </p>' +
         '<div class="weatherNow h4 col-md-2" id="temperaturePlace">  </div>' +
         '</div> ' +
         '<div class="row"> ' +
-        '<p class="h4  col-md-4"> Скорость ветра : </p>' +
-        '<p class="weatherNow h4 col-md-2"> ' + weatherCurrent.wind_kph + ' </p>' +
+        '<p class="h5  col-md-8 col-sm-8 col-xs-8"> Скорость ветра км/ч : </p>' +
+        '<p class="weatherNow h5 col-md-4 col-sm-4 col-xs-4"> ' + weatherCurrent.wind_kph + ' </p>' +
         '</div> ' +
         '<div class="row"> ' +
-        '<p class="h4 h4 col-md-4"> Влажность : </p>' +
-        '<p class="weatherNow h4 col-md-2"> ' + weatherCurrent.humidity + '  </p>' +
+        '<p class="h5  col-md-8 col-sm-8 col-xs-8"> Влажность % : </p>' +
+        '<p class="weatherNow h5 col-md-4 col-sm-4 col-xs-4"> ' + weatherCurrent.humidity + '  </p>' +
         '<div class="weatherNow h4 col-md-2" id="humidityPlace">  </div>' +
+        '</div> ' +
         '</div> ' +
 
         '<div class="col-md-1"></div>' +
-        '<button class="take_weather btn btn-primary "> Обновить</button>'
+        '<button class="take_weather btn btn-primary "> Обновить погоду</button>'
     $('#block').html(weather);
-
 
     /* вставка картинки с влажностью */
     humidity = weatherCurrent.humidity;
     function humidityPic() {
         if (humidity > 80) {
-            console.log(hum);
+           /* console.log(hum);*/
             var humidityHight = '<img  width="50px" height="50px" src="img/raindrop.svg">'
             $('#humidityPlace').html(humidityHight);
         } else {
             var humidityLow = '<img  width="50px" height="50px" src="img/drought.svg">'
-
             $('#humidityPlace').html(humidityLow);
         }
     }
@@ -70,7 +67,7 @@ function renderWeather(data) {
     temperatureFeel = weatherCurrent.temp_c;
     function temperature() {
         if (temperatureFeel > 0) {
-            /*console.log(hum);*/
+
             var humidityHight = '<img  width="50px" height="50px" src="img/thermometer.svg">'
             $('#temperaturePlace').html(humidityHight);
         } else {
@@ -89,9 +86,11 @@ $('#block').on('click', '.take_forecast' , function () {
             weatherForecast.forEach(function (weatherForecast, index) {
             renderForecast(weatherForecast);
         });
-
     });
     cityCurrent();
+    var refreshButton =
+        '<button class="take_forecast forecast_refresh btn btn-primary">Обновить прогноз</button>';
+    $('#block').html(refreshButton);
 });
 
 function renderForecast(data){
@@ -107,12 +106,10 @@ function renderForecast(data){
         '<p class="weatherNow h5 col-md-4 col-sm-4 col-xs-4" > '+ data.day.maxtemp_c +' </p>' +
         '</div> '+
         '<div class="row"> ' +
-        '<p class="h5 col-md-8 col-sm-8 col-xs-8">Скорость ветра : </p>'+
+        '<p class="h5 col-md-8 col-sm-8 col-xs-8">Скорость ветра км/ч : </p>'+
         '<p class="weatherNow h5  col-md-4 "> '+ data.day.maxwind_kph +' </p>' +
         '</div> '+
-        '<button class="take_forecast btn btn-primary">Обновить</button>'+
         '</div>'
-
      $('#block').append(weather);
 
 
